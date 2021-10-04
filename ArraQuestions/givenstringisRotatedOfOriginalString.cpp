56174afabd1a3db1f28d7aebb95e066db6ecd61c
vishal kumar <vishalvlc01@gmail.com>
2021-10-05 00:10:22 +0530

    updated isRotatedString

--- ArraQuestions/givenstringisRotatedOfOriginalString.cpp
+++ ArraQuestions/givenstringisRotatedOfOriginalString.cpp
@@ -7,16 +7,20 @@ int main()
 {
     string s, s1;
     cin >> s >> s1;
+    if(s.length() != s1.length()){
+        cout << "String is not rotated" << endl;
+        return 0;
+    }
     string temp = s + s;
     int findPosn = temp.find(s1);
     if (findPosn != string::npos)
     {
-        cout << "String is rotated"<<endl;
+        cout << "String is rotated" << endl;
         cout << temp << " " << findPosn;
     }
     else
     {
-        cout << "String is not rotated"<<endl;
+        cout << "String is not rotated" << endl;
         cout << temp << " " << findPosn;
     }
 }
---
+++ ArraQuestions/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+if(s.length() != s1.length()){
+    //     cout << "String is not rotated" << endl;
+    //     return 0;
+    // }
